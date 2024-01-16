d99e6d5682d54ba658b9e86b1cd96fa295a6befc
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-01-16 20:58:13 +0100

    [dev] improved ui access page

--- src/login.php
+++ src/login.php
@@ -6,25 +6,21 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <title>Givvit: Login</title>
     </head>
-    <body class="bg-primary-subtle">
-        <main class="container mt-5 p-5 shadow-lg rounded-5 bg-white">
-            <header>
-                <h1>Login</h1>
-            </header>
+    <body class="d-flex justify-content-center py-4 bg-primary-subtle">
+        <main class="mw-75 m-3 p-5 shadow-lg rounded-5 bg-white">
             <form action="#" method="POST">
-                <ul class="list-group">
-                    <li class="list-group-item border-0">
-                        <label for="username">Username:</label>
-                        <input type="text" id="username" name="username" required/>
-                    </li>
-                    <li class="list-group-item border-0">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" required/>
-                    </li>
-                </ul>
-                <label for="login" hidden>Register</label>
-                <input class="btn btn-primary" type="submit" id="login" name="login" value="Login"/>
-                <p>Don't have an account? <a class="text-decoration-none" href="register.php">Sign up</a></p>
+                <h1 class="h3 mb-3 fw-normal">Login</h1>
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" required/>
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" required/>
+                </div>
+                <label for="login" hidden>Login</label>
+                <input class="btn btn-primary w-100 mt-3 mb-2" type="submit" id="login" name="login" value="Login"/>
+                <p>Don't have an account?  <a class="text-decoration-none" href="register.php">Sign up</a></p>
             </form>
         </main>
     </body>
