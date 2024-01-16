fafb4b3ddc31f450b726e12a7cfddd05fe8f7609
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-01-16 12:25:06 +0100

    [dev] improve access page
    removed borders around the input boxes

--- src/login.php
+++ src/login.php
@@ -3,20 +3,21 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">        <title>Givvit: Login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title>Givvit: Login</title>
     </head>
     <body class="bg-primary-subtle">
-        <div class="container mt-5 p-5 shadow-lg rounded-5 bg-white">
+        <main class="container mt-5 p-5 shadow-lg rounded-5 bg-white">
             <header>
                 <h1>Login</h1>
             </header>
             <form action="#" method="POST">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
+                <ul class="list-group">
+                    <li class="list-group-item border-0">
                         <label for="username">Username:</label>
                         <input type="text" id="username" name="username" required/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="password">Password:</label>
                         <input type="password" id="password" name="password" required/>
                     </li>
@@ -25,6 +26,6 @@
                 <input class="btn btn-primary" type="submit" id="login" name="login" value="Login"/>
                 <p>Don't have an account? <a class="text-decoration-none" href="register.php">Sign up</a></p>
             </form>
-        </div>
+        </main>
     </body>
 </html>
--- src/register.php
+++ src/register.php
@@ -3,48 +3,49 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">        <title>Givvit: Login</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title>Givvit: Login</title>
     </head>
     <body class="bg-primary-subtle">
-        <div class="container mt-5 p-5 shadow-lg rounded-5 bg-white">
+        <main class="container mt-5 p-5 shadow-lg rounded-5 bg-white">
             <header>
                 <h1>Register</h1>
             </header>
             <form action="#" method="POST">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">
+                <ul class="list-group">
+                    <li class="list-group-item border-0">
                         <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" required/>
+                        <input type="email" id="email" name="email" autocomplete="off" required/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="name">Name:</label>
-                        <input type="text" id="name" name="name" required/>
+                        <input type="text" id="name" name="name" autocomplete="off" required/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="surname">Surname:</label>
-                        <input type="text" id="surname" name="surname" required/>
+                        <input type="text" id="surname" name="surname" autocomplete="off" required/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="username">Username:</label>
-                        <input type="text" id="username" name="username" required/>
+                        <input type="text" id="username" name="username" autocomplete="off" required/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="password">Password:</label>
-                        <input type="password" id="password" name="password" required/>
+                        <input type="password" id="password" name="password" autocomplete="off" required/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="profile-img">Profile image:</label>
                         <input type="file" id="profile-img" name="profile-img"/>
                     </li>
-                    <li class="list-group-item">
+                    <li class="list-group-item border-0">
                         <label for="desc">Profile description</label>
-                        <input type="textarea" id="desc" name="desc"/>
+                        <input type="textarea" id="desc" name="desc" autocomplete="off"/>
                     </li>
                 </ul>
                 <label for="register" hidden>Register</label>
                 <input class="btn btn-primary" type="submit" id="register" name="register" value="Register"/>
                 <p>Already a member? <a class="text-decoration-none" href="login.php">Sign in</a></p>
             </form>
-        </div>
+        </main>
     </body>
 </html>
