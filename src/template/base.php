a420102f13100e2e791deebb0d653680f6a93ddc
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-01-17 17:12:48 +0100

    [dev] improved template use for homepage

---
+++ src/template/base.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title>Givvit: <?php echo $templateParams["title"]; ?></title>
+    </head>
+    <body class="container justify-content-center py-4 bg-primary-subtle">
+        <!-- Top navbar -->
+        <nav class="nav p-2 fixed-top shadow-sm bg-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col text-start">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" class="bi bi-bell" viewBox="0 0 16 16">
+                            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+                        </svg>
+                    </div>
+                    <div class="col text-center">
+                        <img src="img/logo.png" alt=""Givvit" height="25"/>
+                    </div>
+                    <div class="col text-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" class="bi bi-plus-square" viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- Bottom navbar -->
+        <nav class="d-flex fixed-bottom justify-content-center">
+            <div class="p-3 mb-2 shadow-lg rounded-5 bg-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" class="bi bi-compass border-primary" viewBox="0 0 16 16">
+                    <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016m6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0"/>
+                    <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z"/>
+                </svg>
+                <svg class="ms-3 me-3" xmlns="http://www.w3.org/2000/svg" width="25" class="bi bi-house" viewBox="0 0 16 16">
+                    <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>
+            </div>
+        </nav>
+        <header class="d-flex px-4 mt-5">
+            <h1 class="h2"><?php echo $templateParams["title"]; ?></h1>
+        </header>
+        <main class="container align-items-center justify-content-center">
+            <?php
+                require($templateParams["name"]);
+            ?>
+        </main>
+    </body>
+</html>
---
+++ src/template/show-post.php
@@ -0,0 +1,38 @@
+<?php foreach($templateParams["content"] as $post): ?>
+    <article class="mb-4 p-4 shadow-sm rounded-5 bg-white">
+        <div class="row">
+            <div class="col text-start">
+                <a class="badge link-underline link-underline-opacity-0 bg-primary"><?php echo $post["topic"]; ?></a>
+            </div>
+            <div class="col text-end">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" class="bi bi-star" viewBox="0 0 16 16">
+                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                </svg>
+            </div>
+        </div>
+        <h2 class="h3 my-3"><?php echo $post["short_description"]; ?></h1>
+
+        <!-- If the photo is present:
+            <?php if(isset($post["image"])): ?>
+            <?php echo $templateParams["titolo_pagina"]; ?>
+            <?php endif; ?>
+        -->
+
+        <img src="img/example.jpg" class="img-fluid mb-1" alt="post image"/>
+        <p><?php echo $post["long_description"]; ?></p>
+        <div class="progress" role="progressbar" aria-label="progress with donations" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-bar" style="width: 25%">25%</div>
+        </div>
+        <div class="row mt-2">
+            <div class="col inline text-start">
+                <p>@username</p>
+            </div>
+            <div class="col text-end">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" class="bi bi-chat-left-dots" viewBox="0 0 16 16">
+                    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                    <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                </svg>
+            </div>
+        </div>
+    </article>
+<?php endforeach; ?>
