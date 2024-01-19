d324dccf8b7bf9d7e3b206c385e0a9e5c64264aa
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-01-19 20:32:21 +0100

    [dev] added a filter-by-theme in explore page

--- src/explore.php
+++ src/explore.php
@@ -1,6 +1,10 @@
 <?php
+require_once("db-config.php");
+
 $templateParams["title"] = "Explore";
-$templateParams["name"] = "show-post.php";
+$templateParams["name"] = "show-explore.php";
+$templateParams["posts"] = $dbh->getPostsbyTheme("Science");
+$templateParams["topics"] = $dbh->getTopics();
 
 require("template/base.php");
 ?>
