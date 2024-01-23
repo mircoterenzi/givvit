8a783e277720fd2230dc71c1427c892e5c07ef2b
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-01-23 16:54:56 +0100

    [dev] added user-list for following and followers

---
+++ src/components/user-list-modal.php
@@ -0,0 +1,33 @@
+<?php include_once("db-config.php"); ?>
+<div class="modal fade" id="user-list-modal" tabindex="-1" aria-labelledby="notifications" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <nav>
+                    <a href="#followers" class="me-3">Followers</a>
+                    <a href="#following">Following</a>
+                </nav>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                    <h1 class="h5" id="followers">Followers:</h1>
+                    <ul>
+                        <?php foreach($dbh->getUserFollowers($_GET["id"]) as $user): ?>
+                        <li>
+                            <a class="p-3" href="profile.php?id=<?php echo $user["user_id"]; ?>"><?php echo $user["username"]; ?></a>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <h1 class="h5" id="followers">Following:</h1>
+                    <ul>
+                        <?php foreach($dbh->getFollowedByUser($_GET["id"]) as $user): ?>
+                        <li>
+                            <a class="p-3" href="profile.php?id=<?php echo $user["user_id"]; ?>"><?php echo $user["username"]; ?></a>
+                        </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
