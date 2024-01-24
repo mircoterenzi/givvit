91b4615ccb320e5d85939354332e3019cf549bef
DarioBerti <dario.berti@studio.unibo.it>
2024-01-24 16:56:16 +0100

    [PHP] added js in open-post.php

--- src/open-post.php
+++ src/open-post.php
@@ -2,6 +2,7 @@
 require_once("db-config.php");
 $templateParams["title"] = "Open-post";
 $templateParams["name"] = "show-open-post.php";
+$templateParams["js"] = array("js/star.js");
 $templateParams["post_open"] = $dbh->getPostById($_GET["postId"]);
 
 require("template/base.php");
