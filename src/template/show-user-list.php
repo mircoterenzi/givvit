b85d5c8ee4522d69f755a333e4ba2a236d11859a
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-01-24 15:00:26 +0100

    [fix] user-list without users

--- src/template/show-user-list.php
+++ src/template/show-user-list.php
@@ -1,23 +1,30 @@
-<?php foreach($templateParams["user-list"] as $user): ?>
-    <article class="card mb-4 p-4 shadow-sm rounded-5">
-        <div class="row">
-            <div class="col-2">
-            <div class="ratio ratio-1x1 rounded-circle overflow-hidden">
-                <img src="img/<?php 
-                if(!empty($user["profile_img"])) {
-                echo($user["profile_img"]); 
-                } else {
-                echo("default-propic.jpg");
-                } ?>" alt="profile picture"/>
+<?php if(!empty($templateParams["user-list"])): ?>
+    <?php foreach($templateParams["user-list"] as $user): ?>
+        <article class="card mb-4 p-4 shadow-sm rounded-5">
+            <div class="row">
+                <div class="col-2">
+                <div class="ratio ratio-1x1 rounded-circle overflow-hidden">
+                    <img src="img/<?php 
+                    if(!empty($user["profile_img"])) {
+                    echo($user["profile_img"]); 
+                    } else {
+                    echo("default-propic.jpg");
+                    } ?>" alt="profile picture"/>
+                </div>
+                </div>
+                <div class="col-10">
+                    <a href="profile.php?id=<?php echo $user["user_id"]; ?>" class="fw-bold mb-0">
+                        @<?php echo $user["username"]; ?>
+                    </a>
+                    <p><?php echo $user["first_name"]; ?> <?php echo $user["last_name"]; ?></p>
+                </div>
             </div>
-            </div>
-            <div class="col-10">
-                <a href="profile.php?id=<?php echo $user["user_id"]; ?>" class="fw-bold mb-0">
-                    @<?php echo $user["username"]; ?>
-                </a>
-                <p><?php echo $user["first_name"]; ?> <?php echo $user["last_name"]; ?></p>
-            </div>
-        </div>
-        
-    </article>
-<?php endforeach; ?>
+            
+        </article>
+    <?php endforeach; ?>
+<?php else: ?>
+    <div class="text-center">
+        <p>There are no users in this list :(</p>
+        <p>You can find someone new on the <a href="explore.php">explore page</a>!</p>
+    </div>
+<?php endif; ?>
