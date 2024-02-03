ef7265454c08b3919d17e895a635c870d07aaf6a
DarioBerti <dario.berti@studio.unibo.it>
2024-02-03 23:44:39 +0100

    [PHP] fix show-open-post.php

--- src/template/show-open-post.php
+++ src/template/show-open-post.php
@@ -113,8 +113,8 @@
                     <textarea class="form-control" id="comment" name="inputText" placeholder="Insert your comment"></textarea>
                 </div>
                 <div class="col-lg-3 col-2 d-flex justify-content-end">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-send" viewBox="0 0 20 20">
-                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
+                        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
                     </svg>
                 </div>
             </div>
@@ -125,12 +125,14 @@
                     <?php foreach($comments as $comment): ?>
                         <div class="row align-items-center">
                             <div class="col-2">
+                                <!--icona del proprietario del commento-->
                                 <button type="button" class="btn btn-primary btn-sm rounded-circle" style="width: 28px; height: 28px;">
                                     <i class="fa fa-user"></i>
                                 </button>
                             </div>
                             <div class="col-10">
-                                <article class="card post mb-4 p-4 shadow-sm rounded-5 bg-light">
+                                <!--commento riguardo il post aperto-->
+                                <article class="card post mb-2 p-2 shadow-sm rounded-4 bg-light">
                                     <?php echo "<p>$comment</p><br>";?>
                                 </article>
                             </div>
