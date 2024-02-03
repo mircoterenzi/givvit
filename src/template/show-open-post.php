ff833ecd1361ead3694645de9cddfaaa9cd15f6e
DarioBerti <dario.berti@studio.unibo.it>
2024-02-03 23:49:45 +0100

    [PHP] fixed spacing show-open-post.php

--- src/template/show-open-post.php
+++ src/template/show-open-post.php
@@ -103,7 +103,7 @@
 
             <!--inserimento commento-->
             <div class="row align-items-center">
-                <div class="col-lg-3 col-2 d-flex justify-content-start">
+                <div class="col-lg-3 col-2 d-flex justify-content-end">
                     <!--ICONA FOTO PROFILO DELL'UTENTE LOGGATO IN USO-->
                     <button type="button" class="btn btn-primary btn-sm rounded-circle" style="width: 28px; height: 28px;">
                         <i class="fa fa-user"></i>
@@ -112,7 +112,7 @@
                 <div class="col-lg-6 col-8">
                     <textarea class="form-control" id="comment" name="inputText" placeholder="Insert your comment"></textarea>
                 </div>
-                <div class="col-lg-3 col-2 d-flex justify-content-end">
+                <div class="col-lg-3 col-2 d-flex justify-content-left">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
                         <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
                     </svg>
@@ -124,7 +124,7 @@
                 <?php if(!empty($comments)): ?>
                     <?php foreach($comments as $comment): ?>
                         <div class="row align-items-center">
-                            <div class="col-2">
+                            <div class="col-2 justify-content-right">
                                 <!--icona del proprietario del commento-->
                                 <button type="button" class="btn btn-primary btn-sm rounded-circle" style="width: 28px; height: 28px;">
                                     <i class="fa fa-user"></i>
