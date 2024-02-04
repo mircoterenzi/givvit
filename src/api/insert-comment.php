8c53e54baee01a43220b8d4747172e143fc0c828
DarioBerti <dario.berti@studio.unibo.it>
2024-02-04 12:57:12 +0100

    [JS] fix  input comment in insert-comment.js

--- src/api/insert-comment.php
+++ src/api/insert-comment.php
@@ -1,6 +1,5 @@
 <?php
-
     require_once("../db-config.php");
+    /**inserisco commento nel database */
     $dbh->insertComment($_POST["inputComment"], $_POST["postId"], $_SESSION["userId"]);
-
 ?>
--- src/js/insert-comment.js
+++ src/js/insert-comment.js
@@ -4,13 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     sendButton.forEach(add => {
         add.addEventListener('click', function(){
-        var commentValue = inputComment.value;/**contenuto di testo del commento di input, senza spazi bianchi ad inizio o fine */
-
+        var commentValue = inputComment.value.trim();/**contenuto di testo del commento di input, senza spazi bianchi ad inizio o fine */
         if (commentValue !== '') {
             var formData = new FormData();
             formData.append('inputComment', commentValue);
-            formData.append('postId', add.getAttribute('postid'));
+            formData.append('postId', add.getAttribute('postid'));/**prendo da post id perchè è id proprio del bottone */
             axios.post('./api/insert-comment.php', formData);
+        }else{
+            /**il commento è vuoto e non lo aggiungo al database */
         }
         });
     });
