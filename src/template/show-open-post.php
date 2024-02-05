0b72778754480ab23ad405b61dbb04efbacd745a
DarioBerti <dario.berti@studio.unibo.it>
2024-02-05 18:46:42 +0100

    fixed responses show-open-post.php

--- src/template/show-open-post.php
+++ src/template/show-open-post.php
@@ -153,13 +153,13 @@
                                 <!--controllo se il commento ha una risposta-->
                                 <?php if(!empty($comment["responded_by"])): ?>
                                     <div class = "row justify-content-end">
-                                        <div class="col-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                                        <div class="col-1 ">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
                                             </svg>
                                         </div>
                                         <div class="col-9">
-                                            <article class="card-comment mb-3 p-2 shadow-sm rounded-4 bg-light">
+                                            <article class="card-comment m-3 p-2 shadow-sm rounded-4 bg-light">
                                                 <?php $response = $dbh->getCommentOnPostResponse($post_full["post_id"], $comment["responded_by"]);
                                                     echo $response[0]["text"];
                                                 ?>                                            
