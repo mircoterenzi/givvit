71f156ae59cb074b301f1b78294c3bd796084837
DarioBerti <dario.berti@studio.unibo.it>
2024-02-05 10:08:19 +0100

    Update js array in open-post.php

--- src/open-post.php
+++ src/open-post.php
@@ -2,7 +2,7 @@
 require_once("db-config.php");
 $templateParams["title"] = "Open-post";
 $templateParams["name"] = "show-open-post.php";
-$templateParams["js"] = array("js/notification-viewed.js", "js/star.js", "js/insert-comment.js");
+$templateParams["js"] = array("js/notification-viewed.js", "js/star.js", "js/insert-comment.js", "js/import-donation.js");
 $templateParams["post_open"] = $dbh->getPostById($_GET["postId"]);
 
 require("template/base.php");
