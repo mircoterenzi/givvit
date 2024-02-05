8ec4ae80568dfbc4b5ee476b3a8f8d16f903c849
Coli Alessandro <Alessandro.coli2@studio.unibo.it>
2024-02-05 18:26:49 +0100

    [dev] cannot comment on own post

--- src/api/insert-comment.php
+++ src/api/insert-comment.php
@@ -3,9 +3,13 @@
     $result["insertDone"] = false;
     $result["error"] = "ok";
     /**inserisco commento nel database */
-    if(isset($_POST["inputComment"], $_POST["postId"]) && !empty($_POST["inputComment"])) {
-        $dbh->insertComment($_POST["inputComment"], $_POST["postId"], $_SESSION["userId"]);
-        $result["insertDone"] = true;
+    if(isset($_POST["inputComment"], $_POST["postId"], $_POST["receiver"]) && !empty($_POST["inputComment"])) {
+        if($_POST["receiver"] == $_SESSION["userId"]){
+            $result["error"] = "you can't comment your own post";
+        }else{
+            $dbh->insertComment($_POST["inputComment"], $_POST["postId"], $_SESSION["userId"]);
+            $result["insertDone"] = true;
+        }
     }else{
         $result["error"] = "comment can't be empty";
     }
