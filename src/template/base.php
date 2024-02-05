0eeab25e6260b4ee3670e2638459ffd1c5b6b59e
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-02-05 15:16:24 +0100

    [dev] added cursor change when hovering over icons

--- src/template/base.php
+++ src/template/base.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col text-start pos-relative">
-                        <svg data-bs-toggle="modal" data-bs-target="#notification-modal" xmlns="http://www.w3.org/2000/svg" width="25" class="bi bi-bell" viewBox="0 0 16 16" alt="bell icon">
+                        <svg data-bs-toggle="modal" data-bs-target="#notification-modal" xmlns="http://www.w3.org/2000/svg" width="25" class="clickable bi bi-bell" viewBox="0 0 16 16" alt="bell icon">
                             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
                         </svg>
                         <?php
@@ -40,7 +40,7 @@
                     </div>
                     <div class="col text-end">
                         <div class="d-flex justify-content-end">
-                            <svg data-bs-toggle="modal" data-bs-target="#new-post-modal" xmlns="http://www.w3.org/2000/svg" width="25" class="bi bi-plus-circle" viewBox="0 0 16 16" alt="plus icon">
+                            <svg data-bs-toggle="modal" data-bs-target="#new-post-modal" xmlns="http://www.w3.org/2000/svg" width="25" class="clickable bi bi-plus-circle" viewBox="0 0 16 16" alt="plus icon">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                             </svg>
