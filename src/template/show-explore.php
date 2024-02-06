7b5b77daa264855db3885bc092502842de50bdd2
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-02-06 11:59:34 +0100

    [dev] load more posts

--- src/template/show-explore.php
+++ src/template/show-explore.php
@@ -22,11 +22,11 @@
 </div>
 <?php require("show-post.php"); ?>
 <?php if($n <= sizeof($templateParams["posts"]) ): ?>
-    <div class="container text-center">
-        <?php if(!empty($_GET["topic"])): ?>
-            <a href="explore.php?topic=<?php echo $_GET["topic"]; ?>&n=<?php echo $n+10; ?>" class="btn btn-primary"> Load more </a>
-        <?php else: ?>
-            <a href="explore.php?n=<?php echo $n+10; ?>" class="btn btn-primary"> Load more </a>
-        <?php endif; ?>
-    </div>
+<div class="container text-center">
+    <?php if(!empty($_GET["topic"])): ?>
+        <a href="explore.php?topic=<?php echo $_GET["topic"]; ?>&n=<?php echo $n+10; ?>#<?php echo $n ?>" class="btn btn-primary"> Load more </a>
+    <?php else: ?>
+        <a href="explore.php?n=<?php echo $n+10; ?>#<?php echo $n ?>" class="btn btn-primary"> Load more </a>
     <?php endif; ?>
+</div>
+<?php endif; ?>
