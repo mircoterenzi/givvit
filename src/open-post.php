6ebb0b342414fc9eed7e0c20fef54ed65149d61a
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-02-06 17:02:31 +0100

    [fix] js import

--- src/open-post.php
+++ src/open-post.php
@@ -2,7 +2,7 @@
 require_once("db-config.php");
 $templateParams["title"] = "";
 $templateParams["name"] = "show-open-post.php";
-$templateParams["js"] = array("js/notification-viewed.js", "js/insert-post.js");
+$templateParams["js"] = array("js/notification-viewed.js", "js/insert-post.js", "utils/functions.js");
 $templateParams["post_open"] = $dbh->getPostById($_GET["postId"]);
 $templateParams["topics-list"] = $dbh->getTopics();
 
