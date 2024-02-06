064480a9121812757d280a0588d6a8d5770dbeca
DarioBerti <dario.berti@studio.unibo.it>
2024-02-06 15:42:34 +0100

    [PHP] added delete option on post

--- src/open-post.php
+++ src/open-post.php
@@ -1,6 +1,6 @@
 <?php
 require_once("db-config.php");
-$templateParams["title"] = "Open-post";
+$templateParams["title"] = "";
 $templateParams["name"] = "show-open-post.php";
 $templateParams["js"] = array("js/notification-viewed.js", "js/star.js", "js/insert-comment.js", "js/import-donation.js");
 $templateParams["post_open"] = $dbh->getPostById($_GET["postId"]);
--- src/template/show-open-post.php
+++ src/template/show-open-post.php
@@ -18,19 +18,25 @@
             <?php if($_SESSION["userId"] != $post_full["user_id"]):?>
                 <!--stella SVG-->
                 <div class="col d-flex text-end justify-content-end">
-                <p class="mb-0 me-2"><?php echo $post_full["numLikes"] ?></p>
-                <?php
-                    if(empty($dbh->checkLikeByUser($post_full["post_id"], $_SESSION["userId"]))):
-                ?>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" class="clickable empty-star bi bi-star" viewBox="0 0 16 16" owner-id ="<?php echo $post_full["user_id"]; ?>" post-id ="<?php echo $post_full["post_id"]; ?>" >
-                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                    <p class="mb-0 me-2"><?php echo $post_full["numLikes"] ?></p>
+                    <?php
+                        if(empty($dbh->checkLikeByUser($post_full["post_id"], $_SESSION["userId"]))):
+                    ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" class="clickable empty-star bi bi-star" viewBox="0 0 16 16" owner-id ="<?php echo $post_full["user_id"]; ?>" post-id ="<?php echo $post_full["post_id"]; ?>" >
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                    </svg>
+                    <?php else: ?>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" class="clickable full-star bi bi-star-fill" viewBox="0 0 16 16" value ="<?php echo $post_full["post_id"]; ?>">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>
+                    <?php endif;?>
+                </div>
+            <?php else:?>
+                <div class="col d-flex text-end justify-content-end">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
                 </svg>
-                <?php else: ?>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" class="clickable full-star bi bi-star-fill" viewBox="0 0 16 16" value ="<?php echo $post_full["post_id"]; ?>">
-                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                </svg>
-                <?php endif;?>
-            </div>
+                </div>
             <?php endif;?>
             
         </div>
