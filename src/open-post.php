185ae5eb2066735d49c26294afb34bb8c5ad8a2e
Coli Alessandro <Alessandro.coli2@studio.unibo.it>
2024-02-06 12:25:53 +0100

    [fix] does not load reply.js if there are no comments without reply

--- src/open-post.php
+++ src/open-post.php
@@ -4,6 +4,7 @@ $templateParams["title"] = "Open-post";
 $templateParams["name"] = "show-open-post.php";
 $templateParams["js"] = array("js/notification-viewed.js", "js/star.js", "js/insert-comment.js", "js/import-donation.js");
 $templateParams["post_open"] = $dbh->getPostById($_GET["postId"]);
+$templateParams["topics-list"] = $dbh->getTopics();
 
 if ($templateParams["post_open"]["user_id"] == $_SESSION["userId"] && intval($templateParams["post_open"]["numCom"]) > 0) {
     array_push($templateParams["js"], "js/insert-response.js");
