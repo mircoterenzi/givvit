94384ad858c5bdb2249e86288029213adf39c332
Coli Alessandro <Alessandro.coli2@studio.unibo.it>
2024-02-06 17:38:25 +0100

    html validator for show-post

--- src/js/star.js
+++ src/js/star.js
@@ -6,12 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
         add.addEventListener('click', function(){
             const formData = new FormData();
             formData.append('type', "liked");
-            formData.append('postid', add.getAttribute('post-id'));
+            formData.append('postid', add.getAttribute('data-post-id'));
 
             var notFrom = new FormData();
             notFrom.append("not_type", 'Like');
-            notFrom.append("receiver", add.getAttribute('owner-id'));
-            notFrom.append("post_id", add.getAttribute('post-id'));
+            notFrom.append("receiver", add.getAttribute('data-owner-id'));
+            notFrom.append("post_id", add.getAttribute('data-post-id'));
             axios.post('./api/insert-notification.php',notFrom);
 
             axios.post("./api/star.php",formData).then(() => {
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
         remove.addEventListener('click', function(){
             const formData = new FormData();
             formData.append('type', "not-liked");
-            formData.append('postid', remove.getAttribute('value')); 
+            formData.append('postid', remove.getAttribute('data-post-id')); 
             axios.post("./api/star.php",formData).then(() => {
                 setTimeout(() => location.reload(), 100)
             });
